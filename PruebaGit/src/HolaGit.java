beace8e037c2c6b37505257c889d27d72e3505b7
sergiomf04 <96080742+sergiomf04@users.noreply.github.com>
2022-01-10 19:51:00 +0100

    Update HolaGit.java

--- PruebaGit/src/HolaGit.java
+++ PruebaGit/src/HolaGit.java
@@ -4,6 +4,7 @@ public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("HolaGit!!!");
 		System.out.println("Hoola!!!");
+		System.out.println("Versionremota");
 	}
 
 }
